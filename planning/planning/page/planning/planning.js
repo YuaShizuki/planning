28dde3febac21a6d5ed15b774c86664d44cf9897
Yua Shizuki <keshav@Keshavs-MacBook-Pro.local>
2016-01-15 04:39:41 +0530

    Al

--- planning/planning/page/planning/planning.js
+++ planning/planning/page/planning/planning.js
@@ -1,9 +1,9 @@
-window.open('https://client.loginextsolutions.com/TrackAPack/MasterController?userid=serviceadmin&password=admin&controllerName=LoginController&controllerMethodName=userVerificationProcessRequest&browser=Chrome,46.0.2490.86@remember=me', '_blank').close();
+/*window.open('https://client.loginextsolutions.com/TrackAPack/MasterController?userid=serviceadmin&password=admin&controllerName=LoginController&controllerMethodName=userVerificationProcessRequest&browser=Chrome,46.0.2490.86@remember=me', '_blank').close();
 window.open('https://client.loginextsolutions.com/TrackAPack/MasterController', '_blank');
 $.ajax({url: "", success: function(result){
     console.log("cache cleared")
     window.location = "http://localhost:8000/desk#"
-}});
+}});*/
 /*frappe.pages['planning'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
--- planning/public/js/planning.js
+++ planning/public/js/planning.js
@@ -1,3 +1,10 @@
-window.planning = function () {
-    console.log("Hello World");
+window.onload = function () {
+    $("[data-name=planning]")[0].onclick = function() {
+        w = window.open('https://client.loginextsolutions.com/TrackAPack/MasterController?userid=serviceadmin&password=admin&controllerName=LoginController&controllerMethodName=userVerificationProcessRequest&browser=Chrome,46.0.2490.86@remember=me', '_blank'); 
+        window.setTimeout(function(){
+            w.close()
+            window.open('https://client.loginextsolutions.com/TrackAPack/MasterController', '_blank')
+            window.location = "/desk"
+        }, 1000)
+    }
 }
