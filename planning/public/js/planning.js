524af4cb99852ed7cb05ba34eaabbb0611c3d284
Yua Shizuki <keshav@Keshavs-MacBook-Pro.local>
2016-01-17 17:50:28 +0530

    Hello OWld

--- planning/public/js/planning.js
+++ planning/public/js/planning.js
@@ -3,8 +3,7 @@ window.onload = function () {
         w = window.open('https://client.loginextsolutions.com/TrackAPack/MasterController?userid=serviceadmin&password=admin&controllerName=LoginController&controllerMethodName=userVerificationProcessRequest&browser=Chrome,46.0.2490.86@remember=me', '_blank'); 
         window.setTimeout(function(){
             w.close()
-            window.open('https://client.loginextsolutions.com/TrackAPack/MasterController', '_blank')
-            window.location = "/desk"
+            window.location = 'https://client.loginextsolutions.com/TrackAPack/MasterController'
         }, 3000)
     }
 }
